14f33b48879f25c732dc71d22d2c8b695eed0b04
Uros Pavlovic <uros@brainjocks.com>
2020-09-11 16:32:13 +0200

    Initial commit

---
+++ DockerTestApp/Controllers/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DockerTestApp.Controllers
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int? Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DockerTestApp/Controllers/ProductsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerTestApp.Controllers
+{
+    public class ProductsDbContext: DbContext
+    {
+        
+            public ProductsDbContext(DbContextOptions<ProductsDbContext> options) : base(options)
+            {
+                
+            }
+
+            public DbSet<Product> Products { get; set; }
+
+    }
+}
